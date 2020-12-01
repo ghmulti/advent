d5237efe1ec906de8900a2e02a0b041e7bfd1164
Artur Zabeyvorota <>
2020-12-01 14:45:09 +0100

    cleanup2

--- day1/day1.py
+++ day1/day1.py
@@ -1,5 +1,6 @@
 
 from itertools import combinations, islice
+from functools import reduce 
 
 day1_lines = open("./day1/input.txt")
 lines = day1_lines.readlines()
@@ -8,15 +9,14 @@ def conv(lst):
     for e in lst:
         yield int(e.strip())
 
-
+def calc(lines, amount):
+    mapped_lines = combinations(conv(lines), amount)
+    return (entry for entry in mapped_lines if sum(entry) == 2020)
+    
 ###### part 1
-lines_comb_2 = combinations(lines, 2)
-mapped_lines_2 = (list(conv(line)) for line in lines_comb_2)
-passed_2 = ([entry, entry[0] * entry[1]] for entry in mapped_lines_2 if sum(entry) == 2020)
-print(next(passed_2))
+passed_2 = next(calc(lines, 2))
+print(passed_2, reduce(lambda x,y: x * y, passed_2))
 
 ###### part 2
-lines_comb_3 = combinations(lines, 3)
-mapped_lines_3 = (list(conv(line)) for line in lines_comb_3)
-passed_3 = ([entry, entry[0] * entry[1] * entry[2]] for entry in mapped_lines_3 if sum(entry) == 2020)
-print(next(passed_3))
+passed_3 = next(calc(lines, 3))
+print(passed_3, reduce(lambda x,y: x * y, passed_3))
