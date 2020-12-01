74334c830c90c333f890ca37dccd51a8d95e39ef
Artur Zabeyvorota <>
2020-12-01 14:36:36 +0100

    cleanup

--- day1/day1.py
+++ day1/day1.py
@@ -11,12 +11,12 @@ def conv(lst):
 
 ###### part 1
 lines_comb_2 = combinations(lines, 2)
-mapped_lines_2 = (list(islice(conv(line), 2)) for line in lines_comb_2)
-passed_2 = ([entry, entry[0] * entry[1]] for entry in mapped_lines_2 if entry[0] + entry[1] == 2020)
+mapped_lines_2 = (list(conv(line)) for line in lines_comb_2)
+passed_2 = ([entry, entry[0] * entry[1]] for entry in mapped_lines_2 if sum(entry) == 2020)
 print(next(passed_2))
 
 ###### part 2
 lines_comb_3 = combinations(lines, 3)
-mapped_lines_3 = (list(islice(conv(line), 3)) for line in lines_comb_3)
-passed_3 = ([entry, entry[0] * entry[1] * entry[2]] for entry in mapped_lines_3 if entry[0] + entry[1] + entry[2] == 2020)
+mapped_lines_3 = (list(conv(line)) for line in lines_comb_3)
+passed_3 = ([entry, entry[0] * entry[1] * entry[2]] for entry in mapped_lines_3 if sum(entry) == 2020)
 print(next(passed_3))
