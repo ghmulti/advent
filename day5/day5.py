a4ab5d173ffe8936c78ded0cb4f331ccebd54d2f
Artur Zabeyvorota <>
2020-12-05 09:00:23 +0100

    day5 v2

--- day5/day5.py
+++ day5/day5.py
@@ -2,30 +2,23 @@
 lines = open("./day5/input.txt").readlines()
 # print(lines)
 
-def calculate_position(line):
-    pos = [0, 127]
-    for i in range(7):
+def binary_search(power, line, lower_predicate):
+    pos = [0, 2**power - 1]    
+    for i in range(power):
         letter = line[i]
         diff = (pos[1] - pos[0] + 1)//2
-        if letter == "F":
+        if lower_predicate(letter):
             pos = [pos[0], pos[1]-diff]
         else:
             pos = [pos[0]+diff, pos[1]]
         # print(pos)
-    assert pos[0] == pos[1]
+    assert pos[0] == pos[1], (f"Not equals for line {line} for power {power}", pos[0], pos[1])
+    return pos[0]
 
-    pos2 = [0, 7]
-    for i in range(3):
-        letter = line[i + 7]
-        diff = (pos2[1] - pos2[0] + 1)//2
-        if letter == "L":
-            pos2 = [pos2[0], pos2[1]-diff]
-        else:
-            pos2 = [pos2[0]+diff, pos2[1]]
-        # print(pos2)
-    assert pos2[0] == pos2[1]            
-
-    return [pos[0],pos2[0]]
+def calculate_position(line):
+    row = binary_search(power=7, line=line[:7], lower_predicate=lambda letter: letter == "F")
+    column = binary_search(power=3, line=line[7:], lower_predicate=lambda letter: letter == "L")
+    return [row, column]
 
 assert calculate_position("FBFBBFFRLR") ==  [44,5]
 assert calculate_position("BFFFBBFRRR") ==  [70,7]
@@ -41,24 +34,26 @@ occupied_positions = list(calculate_position(line.strip()) for line in lines)
 occupied_seat_ids = list(seat_id(position) for position in occupied_positions)
 # print(occupied_seat_ids)
 
+
 #### part 1
 max_occupied_seat_id = max(occupied_seat_ids)
 print(f"Max occupied seat id {max_occupied_seat_id}")
 
+
+#### part 2
 def all_positions():
     for row in range(128):
-        if row <= 9 or row >= 116:
-            continue
         for col in range(8):
             yield [row, col]
 
-all_positions = list(all_positions())
-free_positions = list(position for position in all_positions if position not in occupied_positions)
-print(free_positions)
-# print(len(free_positions))
+free_positions = list(p for p in all_positions() if p not in occupied_positions)
+# print(free_positions)
+free_positions_filtered = list(p for p in free_positions if [p[0], p[1]+1] not in free_positions and [p[0], p[1]-1] not in free_positions)
+# print(free_positions_filtered)
 
-free_seat_ids = list(seat_id(free_position) for free_position in free_positions)
+free_seat_ids = list(seat_id(free_position) for free_position in free_positions_filtered)
 # print(free_seat_ids)
+free_seat_ids_filtered = list(fsi for fsi in free_seat_ids if fsi+1 in occupied_seat_ids and fsi-1 in occupied_seat_ids)
 
-free_seat_ids_filtered = list(free_seat_id for free_seat_id in free_seat_ids if free_seat_id+1 in occupied_seat_ids and free_seat_id-1 in occupied_seat_ids)
-print(free_seat_ids_filtered)
+assert len(free_seat_ids_filtered) == 1
+print(f"Your seat id is {free_seat_ids_filtered[0]}")
