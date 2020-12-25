f4bbb3eab7c120f9dd67109638ebbc7a892a9fe8
Artur Zabeyvorota <>
2020-12-25 20:27:46 +0100

    day23 part2

--- day23/day23.py
+++ day23/day23.py
@@ -40,62 +40,69 @@ print(f"Answer = {answer_1}")
 
 print("==== Part 2 ====")
 
+class Node:
+    def __init__(self, value):
+        self.next = self.prev = None
+        self.value = value
 
-# def select_destination(pick, current_cup, lowest, highest):
-#     while True:
-#         current_cup = current_cup - 1
-#         if current_cup < lowest:
-#             current_cup = highest        
-#         if current_cup not in pick:
-#             return current_cup
+class LinkedListWithCache:
+    def __init__(self, values):
+        self.cache = {}
+        head = prev = None
+        for value in values:
+            node = Node(value)
+            if not head:
+                head = node
+            if prev:
+                prev.next = node
+                node.prev = prev
+            self.cache[value] = prev = node
+        head.prev = prev
+        prev.next = head
+    
+    def get(self, key):
+        return self.cache[key]
 
-# def move(cups, limit, steps):
-#     print(f"Working with {cups}")
-#     source_cups = cups[::]
-#     cups = cups + list(range(max(cups) + 1, limit + 1))
-#     current_index = 0
-#     current_step = 0
-#     subindex_1 = []
-#     subindex_2 = []
-#     last_pick = None
-#     last_el = None
-#     current_index = 0       
-#     while current_step < steps:
-#         current_step += 1
+    def move(self, source, dest):
+        item, destination = self.cache[source], self.cache[dest]
+        prev_next = destination.next
+        item.prev.next = item.next
+        item.next.prev = item.prev
+        destination.next.prev = item
+        destination.next = item
+        item.prev = destination
+        item.next = prev_next
 
-#         if last_el and cups[current_index] - last_pick == 1:
-#             subindex_1.append(cups[current_index])
-#             subindex_2.append(cups[current_index+1])
-#             subindex_2.append(cups[current_index+2])
-#             subindex_2.append(cups[current_index+3])
-#             last_pick = subindex_2[-1]
-#             current_index += 4
-#             continue
-#         else:
-#             if subindex_1:
-#                 destination_index = select_destination(subindex_2, subindex_1[0], 1, limit)
-#                 cups = cups[current_index:destination_index+1] + subindex_2 + cups[destination_index+1:] + subindex_1
-#                 current_index = 0
-#                 subindex_1 = []
-#                 subindex_2 = []
-#                 last_pick = None
-#                 continue
+def select_destination_wrapper(lowest, highest):
+    def fn(pick, current_cup):
+        return select_destination(pick, current_cup, lowest, highest)
+    return fn
 
-#         current_cup = cups.pop(0)
-#         pick = [cups.pop(0), cups.pop(0), cups.pop(0)]
-#         last_pick = pick[-1]
-#         last_el = current_cup
-#         destination = select_destination(pick, current_cup, 1, limit)
-#         destination_index = cups.index(destination)    
-#         cups = cups[:destination_index+1] + pick + cups[destination_index+1:] + [current_cup]                    
-#         print(f"[{current_step}] Current cup {current_cup}, pick {pick} {cups}")        
+def run(cups, limit, steps):
+    print(f"Working with {cups}")
+    next_key = cups[0]
+    cups = LinkedListWithCache(cups + list(range(max(cups) + 1, limit + 1)))
+    current_step = 0
+    select_destination_fn = select_destination_wrapper(1, limit)
+    while current_step < steps:
+        current_step += 1
+        item = cups.get(next_key)
+        tmp_next_key = item.next.next.next.next.value
+        pick = [item.next.value, item.next.next.value, item.next.next.next.value]
+        destination_value = select_destination_fn(pick, item.value)
+        for p in pick[::-1]:
+            cups.move(p, destination_value)
+        next_key = tmp_next_key
+    return cups
 
-#     return cups
+cups = list(int(e) for e in "739862541")
+result = run(cups=cups, limit=1000000, steps=10000000)
+index_one = result.get(1)
+# print(f"Element 1 { index_one.next.value }")
+# print(f"Element 2 { index_one.next.next.value }")
 
-# cups = list(int(e) for e in "389125467")
-# # result = move(cups=cups, limit=1000000, steps=10000000)
-# result = move(cups=cups, limit=9, steps=10)
-# print(result)
-# index_one = result.index(1)
-# print(f"Element 1 { result[index_one + 1] }")
-# print(f"Element 2 { result[index_one + 2] }")
+answer_2 = index_one.next.value * index_one.next.next.value
+
+print(f"Answer = {answer_2}")
+
+assert answer_2 == 3072905352
